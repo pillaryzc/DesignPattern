aa4771e6a25fa29ac2328572aead6466fc9c210c
pillar <pillaryzc@163.com>
2023-11-22 01:33:36 +0800

    Waker

--- src/closure/future_explain_in_200_line.rs
+++ src/closure/future_explain_in_200_line.rs
@@ -1,4 +1,4 @@
-/* 
+/*
 绿色线程(Green threads)
 绿色线程使用与操作系统相同的机制，为每个任务创建一个线程，设置一个堆栈，保存 CPU 状态，并通过“上下文切换”从一个任务(线程)跳转到另一个任务(线程)。
 
@@ -17,7 +17,7 @@ Rust曾经支持绿色线程，但他们它达到1.0之前被删除了， 执行
 如果您想要支持许多不同的平台，就很难正确实现
 
 */
-/* 
+/*
  #![feature(asm, naked_functions)]
 use std::ptr;
 
@@ -121,7 +121,7 @@ impl Runtime {
                 return false;
             }
         }
-        
+
         if self.threads[self.current].state != State::Available {
             self.threads[self.current].state = State::Ready;
         }
@@ -144,7 +144,7 @@ impl Runtime {
                 .iter_mut()
                 .find(|t| t.state == State::Available)
                 .expect("no available thread.");
-                
+
             let size = available.stack.len();
             let s_ptr = available.stack.as_mut_ptr();
             available.task = Some(Box::new(f));
@@ -248,23 +248,14 @@ fn main() {
 
 */
 
-
-
-
-
-
-
-
-
-
-/* 
+/*
 基于回调的方法背后的整个思想就是保存一个指向一组指令的指针，这些指令我们希望以后在以后需要的时候运行。 针对Rust，这将是一个闭包。 在下面的示例中，我们将此信息保存在HashMap中，但这并不是唯一的选项。
 
 不涉及线程作为实现并发性的主要方法的基本思想是其余方法的共同点。 包括我们很快就会讲到的 Rust 今天使用的那个。
 
-优点: 
-1. 大多数语言中易于实现 
-2. 没有上下文切换 
+优点:
+1. 大多数语言中易于实现
+2. 没有上下文切换
 3. 相对较低的内存开销(在大多数情况下)
 
 缺点:
@@ -275,6 +266,8 @@ fn main() {
 4.在 Rust 使用这种方法时，任务之间的状态共享是一个难题，因为它的所有权模型
 一个极其简单的基于回调方法的例子是:
 */
+
+use crate::main;
 
 #[test]
 fn test_callback_programming() {
@@ -346,3 +339,117 @@ fn test_callback_programming() {
         }
     }
 }
+
+
+/* Waker 唤醒器
+1.了解 Waker 对象是如何构造的
+2.了解运行时如何知道leaf-future何时可以恢复
+3.了解动态分发的基础知识和trait对象
+*/
+
+/* 1.了解Waker,先看胖指针
+&dyn Trait:-----16   前8个字节指向trait 对象的data - 后八个字节指向trait对象的 vtable
+&[&dyn Trait]:--16
+Box<Trait>:-----16
+&i32:-----------8
+&[i32]:---------16   前8个字节是指向数组中第一个元素的实际指针(或 slice 引用的数组的一部分) - 第二个8字节是切片的长度
+Box<i32>:-------8
+&Box<i32>:------8
+[&dyn Trait;4]:-64
+[i32;4]:--------16
+*/
+#[test]
+fn test_fat_prt() {
+    use std::mem::size_of;
+
+    trait SomeTrait {}
+
+    fn main() {
+        println!("======== The size of different pointers in Rust: ========");
+        println!("&dyn Trait:-----{}", size_of::<&dyn SomeTrait>());
+        println!("&[&dyn Trait]:--{}", size_of::<&[&dyn SomeTrait]>());
+        println!("Box<Trait>:-----{}", size_of::<Box<dyn SomeTrait>>());
+        println!("&i32:-----------{}", size_of::<&i32>());
+        println!("&[i32]:---------{}", size_of::<&[i32]>());
+        println!("Box<i32>:-------{}", size_of::<Box<i32>>());
+        println!("&Box<i32>:------{}", size_of::<&Box<i32>>());
+        println!("[&dyn Trait;4]:-{}", size_of::<[&dyn SomeTrait; 4]>());
+        println!("[i32;4]:--------{}", size_of::<[i32; 4]>());
+    }
+
+    main()
+}
+
+/* 虚表的数据结构 */
+#[test]
+fn costom_fat_ptr() {
+    // A reference to a trait object is a fat pointer: (data_ptr, vtable_ptr)
+    trait Test {
+        fn add(&self) -> i32;
+        fn sub(&self) -> i32;
+        fn mul(&self) -> i32;
+    }
+
+    // This will represent our home brewn fat pointer to a trait object
+    #[repr(C)]
+    struct FatPointer<'a> {
+        /// A reference is a pointer to an instantiated `Data` instance
+        data: &'a mut Data,
+        /// Since we need to pass in literal values like length and alignment it's
+        /// easiest for us to convert pointers to usize-integers instead of the other way around.
+        vtable: *const usize,
+    }
+
+    // This is the data in our trait object. It's just two numbers we want to operate on.
+    struct Data {
+        a: i32,
+        b: i32,
+    }
+
+    // ====== function definitions ======
+    fn add(s: &Data) -> i32 {
+        s.a + s.b
+    }
+    fn sub(s: &Data) -> i32 {
+        s.a - s.b
+    }
+    fn mul(s: &Data) -> i32 {
+        s.a * s.b
+    }
+
+    fn main() {
+        let mut data = Data { a: 3, b: 2 };
+        // vtable is like special purpose array of pointer-length types with a fixed
+        // format where the three first values has a special meaning like the
+        // length of the array is encoded in the array itself as the second value.
+        let vtable = vec![
+            0, // pointer to `Drop` (which we're not implementing here)
+            6, // lenght of vtable
+            8, // alignment
+            // we need to make sure we add these in the same order as defined in the Trait.
+            add as usize, // function pointer - try changing the order of `add`
+            sub as usize, // function pointer - and `sub` to see what happens
+            mul as usize, // function pointer
+        ];
+
+        let fat_pointer = FatPointer {
+            data: &mut data,
+            vtable: vtable.as_ptr(),
+        };
+        let test = unsafe { std::mem::transmute::<FatPointer, &dyn Test>(fat_pointer) };
+
+        // And voalá, it's now a trait object we can call methods on
+        println!("Add: 3 + 2 = {}", test.add());
+        println!("Sub: 3 - 2 = {}", test.sub());
+        println!("Mul: 3 * 2 = {}", test.mul());
+    }
+
+    main();
+}
+
+
+/* 生成器
+1.理解 async / await 语法在底层是如何工作的
+2/亲眼目睹(See first hand)我们为什么需要Pin
+3.理解是什么让 Rusts 异步模型的内存效率非常高
+*/
