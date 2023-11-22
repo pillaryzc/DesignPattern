c9daa683cd7ca0c18eda1bb2c9aa38616708537e
pillar <pillaryzc@163.com>
2023-11-23 01:05:52 +0800

    practice pin data in heap

--- src/closure/future_explain_in_200_line.rs
+++ src/closure/future_explain_in_200_line.rs
@@ -267,6 +267,8 @@ fn main() {
 一个极其简单的基于回调方法的例子是:
 */
 
+use std::{marker::PhantomPinned, pin::Pin};
+
 use crate::main;
 
 #[test]
@@ -723,5 +725,171 @@ fn pinning_to_heap() {
 
         println!("a: {}, b: {}", test1.as_ref().a(), test1.as_ref().b());
         println!("a: {}, b: {}", test2.as_ref().a(), test2.as_ref().b());
+    }
+}
+
+// todo!()
+#[test]
+fn test_pin_in_heap() {
+    use std::pin::Pin;
+
+    struct Test {
+        to_borrow: String,
+        borrow: *const String,
+        _marker: PhantomPinned,
+    }
+    impl Test {
+        fn new(s: &str) -> Pin<Box<Self>> {
+            let mut t = Test {
+                to_borrow: String::from(s),
+                borrow: std::ptr::null(),
+                _marker: PhantomPinned,
+            };
+            let mut boxed = Box::pin(t);
+            let self_ptr: *const String = &boxed.as_ref().to_borrow;
+            unsafe {
+                boxed.as_mut().get_unchecked_mut().borrow = self_ptr;
+            }
+            boxed
+        }
+
+        fn a<'a>(self: Pin<&'a Self>) -> &'a str {
+            &self.get_ref().to_borrow
+        }
+
+        fn b<'a>(self: Pin<&'a Self>) -> &'a String {
+            unsafe { &*(self.borrow) }
+        }
+    }
+
+    fn main() {
+        let mut test1 = Test::new("test1");
+        let mut test2 = Test::new("test2");
+
+        println!("a: {}, b: {}", test1.as_ref().a(), test1.as_ref().b());
+        std::mem::swap(&mut test1, &mut test2);
+        println!("a: {}, b: {}", test2.as_ref().a(), test2.as_ref().b());
+    }
+
+    main()
+}
+
+/* 学习了pin,现在可以来解决之前生成器的自引用问题了 */
+fn pin_fix_generator() {
+    #![feature(auto_traits, negative_impls)] // needed to implement `!Unpin`
+    use std::pin::Pin;
+
+    pub fn main() {
+        let gen1 = GeneratorA::start();
+        let gen2 = GeneratorA::start();
+        // Before we pin the data, this is safe to do
+        // std::mem::swap(&mut gen, &mut gen2);
+
+        // constructing a `Pin::new()` on a type which does not implement `Unpin` is
+        // unsafe. An object pinned to heap can be constructed while staying in safe
+        // Rust so we can use that to avoid unsafe. You can also use crates like
+        // `pin_utils` to pin to the stack safely, just remember that they use
+        // unsafe under the hood so it's like using an already-reviewed unsafe
+        // implementation.
+
+        let mut pinned1 = Box::pin(gen1);
+        let mut pinned2 = Box::pin(gen2);
+
+        // Uncomment these if you think it's safe to pin the values to the stack instead
+        // (it is in this case). Remember to comment out the two previous lines first.
+        //let mut pinned1 = unsafe { Pin::new_unchecked(&mut gen1) };
+        //let mut pinned2 = unsafe { Pin::new_unchecked(&mut gen2) };
+
+        if let GeneratorState::Yielded(n) = pinned1.as_mut().resume() {
+            println!("Gen1 got value {}", n);
+        }
+
+        if let GeneratorState::Yielded(n) = pinned2.as_mut().resume() {
+            println!("Gen2 got value {}", n);
+        };
+
+        // This won't work:
+        // std::mem::swap(&mut gen, &mut gen2);
+        // This will work but will just swap the pointers so nothing bad happens here:
+        // std::mem::swap(&mut pinned1, &mut pinned2);
+
+        let _ = pinned1.as_mut().resume();
+        let _ = pinned2.as_mut().resume();
+    }
+
+    enum GeneratorState<Y, R> {
+        Yielded(Y),
+        Complete(R),
+    }
+
+    trait Generator {
+        type Yield;
+        type Return;
+        fn resume(self: Pin<&mut Self>) -> GeneratorState<Self::Yield, Self::Return>;
+    }
+
+    enum GeneratorA {
+        Enter,
+        Yield1 {
+            to_borrow: String,
+            borrowed: *const String,
+        },
+        Exit,
+    }
+
+    impl GeneratorA {
+        fn start() -> Self {
+            GeneratorA::Enter
+        }
+    }
+
+    // This tells us that this object is not safe to move after pinning.
+    // In this case, only we as implementors "feel" this, however, if someone is
+    // relying on our Pinned data this will prevent them from moving it. You need
+    // to enable the feature flag `#![feature(optin_builtin_traits)]` and use the
+    // nightly compiler to implement `!Unpin`. Normally, you would use
+    // `std::marker::PhantomPinned` to indicate that the struct is `!Unpin`.
+    // impl !Unpin for GeneratorA {}
+
+    impl Generator for GeneratorA {
+        type Yield = usize;
+        type Return = ();
+        fn resume(self: Pin<&mut Self>) -> GeneratorState<Self::Yield, Self::Return> {
+            // lets us get ownership over current state
+            let this = unsafe { self.get_unchecked_mut() };
+            match this {
+                GeneratorA::Enter => {
+                    let to_borrow = String::from("Hello");
+                    let borrowed = &to_borrow;
+                    let res = borrowed.len();
+                    *this = GeneratorA::Yield1 {
+                        to_borrow,
+                        borrowed: std::ptr::null(),
+                    };
+
+                    // Trick to actually get a self reference. We can't reference
+                    // the `String` earlier since these references will point to the
+                    // location in this stack frame which will not be valid anymore
+                    // when this function returns.
+                    if let GeneratorA::Yield1 {
+                        to_borrow,
+                        borrowed,
+                    } = this
+                    {
+                        *borrowed = to_borrow;
+                    }
+
+                    GeneratorState::Yielded(res)
+                }
+
+                GeneratorA::Yield1 { borrowed, .. } => {
+                    let borrowed: &String = unsafe { &**borrowed };
+                    println!("{} world", borrowed);
+                    *this = GeneratorA::Exit;
+                    GeneratorState::Complete(())
+                }
+                GeneratorA::Exit => panic!("Can't advance an exited generator!"),
+            }
+        }
     }
 }
